592f73b5bb4efcf4c8d26f73b9ddd6d7735f12a7
SurgeousJP <tuanbao2311@gmail.com>
2024-12-16 15:48:17 +0700

    Update 2

--- src/ChatbotAPI/app/environment.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-# Access environment variables
-HF_TOKEN = os.getenv("HF_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
